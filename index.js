47ce29e845a8f25416358ad610789bc3ca7e7402
UmidYul <yumid253@gmail.com>
2025-10-04 19:12:29 +0500

    first deploy111

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname+"/views/tech.html");
 });
 
+app.get('/main', (req, res) => {
+	res.sendFile(__dirname+"/views/main.html");
+});
+app.get('/achieve', (req, res) => {
+	res.sendFile(__dirname+"/views/achievements_contact.html");
+});
+app.get('/teach', (req, res) => {
+	res.sendFile(__dirname+"/views/teacher_about.html");
+});
+
 app.listen(PORT, () => {
 	console.log(`http://localhost:${PORT}`);
 });
