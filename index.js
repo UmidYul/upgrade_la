c2cffeb223447ecb705a445ab000dd05c7166d11
UmidYul <yumid253@gmail.com>
2025-10-04 22:38:12 +0500

    124256568887

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { Telegraf } from "telegraf";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const bot = new Telegraf('8302681013:AAGjxusG2zhbM8_hguH4mj5L2-BFaDN8S7Y');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,12 +21,24 @@ app.get('/', (req, res) => {
 app.get('/main', (req, res) => {
 	res.sendFile(__dirname+"/views/main.html");
 });
-app.get('/achieve', (req, res) => {
-	res.sendFile(__dirname+"/views/achievements_contact.html");
-});
-app.get('/teach', (req, res) => {
-	res.sendFile(__dirname+"/views/teacher_about.html");
-});
+
+app.post('/form', (req, res) => {
+	const{name, email, message,phone} = req.body;
+	  bot.telegram.sendMessage(8176192068, `
+<b>📋 New Application!:</b>
+
+<b>Name:</b>
+${name || "-"}
+<b>Email:</b>
+${email || "-"}
+<b>Phone:</b>
+${phone || "-"}
+<b>Message:</b>
+<blockquote>${message || "-"}</blockquote>
+        `, {
+            parse_mode: "HTML"
+        });
+})
 
 app.listen(PORT, () => {
 	console.log(`http://localhost:${PORT}`);
